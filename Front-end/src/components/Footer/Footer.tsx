e2642042cfb844e5a9d62edf37d3e0143afbcf7a
Phạm Đức <p.mduc.ptit@gmail.com>
2025-11-27 14:12:59 +0700

    Add Footer with tailwind, remove Footer.css

--- Front-end/src/components/Footer/Footer.tsx
+++ Front-end/src/components/Footer/Footer.tsx
@@ -1,13 +1,11 @@
-import './Footer.css';
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="footer">
-      <div className="footer-bottom">
+    <footer className="bg-[#001529] text-white text-sm pt-[6px] px-[1px] pb-[1px]">
+      <div className="max-w-[1200px] mx-auto border-t border-[#ffffff1a] text-center text-[#6c757d]">
         <p>&copy;{currentYear} Copyright 2025 Igata Kuraitoku. All rights reserved.</p>
-        <p>Liên hệ: <a href='mailto:p.mduc.ptit@gmail.com' style={{textDecoration:'none', color:'#1877f2'}}>p.mduc.ptit@gmail.com</a></p>
+        <p>Liên hệ: <a href='mailto:p.mduc.ptit@gmail.com' className="no-underline text-[#1877f2] hover:underline">p.mduc.ptit@gmail.com</a></p>
       </div>
     </footer>
   );
